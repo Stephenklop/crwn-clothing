2348aba79910d676caf1eb67ac15f5e4916e5f38
Stephen Klop <stefklop18@gmail.com>
2021-07-18 01:36:37 +0200

    Added google oAuth

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyA7lUJPuQ7Y_7Rdk6ZQBbF2Yq07-olGPh8",
+  authDomain: "crwn-clothing-db-5a2f5.firebaseapp.com",
+  projectId: "crwn-clothing-db-5a2f5",
+  storageBucket: "crwn-clothing-db-5a2f5.appspot.com",
+  messagingSenderId: "502129473266",
+  appId: "1:502129473266:web:44bc2cb32e9a9c94ab826b",
+  measurementId: "G-ZZFMKTC6Y8",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
